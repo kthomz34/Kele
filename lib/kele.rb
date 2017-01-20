19180d70640e5208b87212693c81d265a5438ef3
Kurt Thomas <kurt.thomas34@gmail.com>
2017-01-20 04:36:31 +0000

    checkpoint 2 & 3 complete

---
+++ lib/kele.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+
+class Kele
+
+  include HTTParty
+
+
+  def initialize(email, password)
+		response = self.class.post(api_end_point('/sessions'), body: {email: email, password: password})
+		@auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(api_end_point('/users/me'), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+  end
+
+  private
+
+  def api_end_point(end_point)
+    "https://www.bloc.io/api/v1#{end_point}"
+  end
+end
