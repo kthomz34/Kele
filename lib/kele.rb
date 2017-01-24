512d3212ea20f3db830d5d46e99d011f806e11ce
Kurt Thomas <kurt.thomas34@gmail.com>
2017-01-24 16:07:50 -0500

    added mentor availability (#2)

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,13 +6,18 @@ class Kele
 
 
   def initialize(email, password)
-		response = self.class.post(api_end_point('/sessions'), body: {email: email, password: password})
+		response = self.class.post(api_end_point('/sessions'), body: { email: email, password: password })
 		@auth_token = response["auth_token"]
   end
 
   def get_me
     response = self.class.get(api_end_point('/users/me'), headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
+  end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_end_point("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 
   private
