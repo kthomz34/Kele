15cd88a210cedf0cd131de1cf5637d82d8131e62
Kurt Thomas <kurt.thomas34@gmail.com>
2017-01-24 23:27:55 +0000

    checkpoint 5 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,6 +19,16 @@ class Kele
     response = self.class.get(api_end_point("/mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
   end
+  
+  def get_roadmap(roadmap_id)
+    response = self.class.get(api_end_point("/roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)
+  end
+  
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(api_end_point("/checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)
+  end
 
   private
 
