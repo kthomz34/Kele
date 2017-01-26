753b0d0fcb27cd921bdeffec3cc0036c6f391066
Kurt Thomas <kurt.thomas34@gmail.com>
2017-01-26 13:29:52 -0500

    checkpoint 6 complete (#4)

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,6 +29,16 @@ class Kele
     response = self.class.get(api_end_point("/checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
     @checkpoint = JSON.parse(response.body)
   end
+  
+  def get_messages(messages)
+    response = self.class.get(api_end_point('/message_threads'), headers: { "authorization" => @auth_token })
+    @message = JSON.parse(response.body)
+  end
+  
+  def create_message(sender, recipient_id, token, subject, stripped_text)
+    response = self.class.post(api_end_point('/messages'), body: { sender: sender, recipient_id: recipient_id, token: token, subject: subject, "stripped-text": stripped_text} , headers: { "authorization" => @auth_token })
+    @create_message = JSON.parse(response.body)
+  end
 
   private
 
